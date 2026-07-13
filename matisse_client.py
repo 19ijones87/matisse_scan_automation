d3781880f0bc27eff884e1e0602498c11c271244
19ijones87 <hceylan22@ku.edu.tr>
2026-07-13 19:26:35 +0200

    send_command func commpleted

--- matisse_client.py
+++ matisse_client.py
@@ -13,6 +13,8 @@ Author: A. Halil Ceylan
 
 import socket
 
+COMMAND_LENGTH_BYTES = 4 #in bytes
+
 def connect_to_matisse(host, port, timeout = 1.0):
     s = socket.socket()
     s.settimeout(timeout)
@@ -40,8 +42,16 @@ def disconnect_from_matisse(sock):
     sock.close()
 
 def send_command(sock, command):
-    pass
+    command_packet_bytes = b""
 
+    command_bytes = command.encode()
+
+    command_len = len(command)
+    command_len_bytes = command_len.to_bytes(COMMAND_LENGTH_BYTES, "big")
+
+    command_packet_bytes += (command_len_bytes + command_bytes)
+
+    sock.sendall(command_packet_bytes)
 def receive_exact_bytes(sock, n):
     pass
 
